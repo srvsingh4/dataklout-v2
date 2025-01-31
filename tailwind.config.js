67d238ed8eb215ccc91d2388befccca96ac217ee
srvsingh4 <131736167+srvsingh4@users.noreply.github.com>
2025-01-31 17:51:20 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  build: {
-    target: "esnext", // Ensure modern module support
-  },
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
@@ -10,10 +7,5 @@ export default {
   plugins: [],
   corePlugins: {
     apply: true,
-  },
-  server: {
-    mimeTypes: {
-      js: "text/javascript",
-    },
   },
 };
